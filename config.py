dc0f3aec14ac1fc1e59d053b7cee43ba339ec69c
neomneom11 <neomneom11q@gmail.com>
2025-05-29 19:34:19 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    CANOPY_API_KEY = "your-canopy-api-key"
-    WORDPRESS_API_KEY = "your-wordpress-api-key"
-    WORDPRESS_USERNAME = "your-wordpress-username"
-    WORDPRESS_URL = "https://yourwordpresssite.com"
-    MARKUP_PERCENTAGE = 30
