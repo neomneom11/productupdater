bd749b8cd8e6a8894f4c831df24881653e9ca52c
neomneom11 <neomneom11q@gmail.com>
2025-05-29 19:40:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    CANOPY_API_KEY = "your-canopy-api-key"
-    WORDPRESS_API_KEY = "your-wordpress-api-key"
-    WORDPRESS_USERNAME = "your-wordpress-username"
-    WORDPRESS_URL = "https://yourwordpresssite.com"
+    CANOPY_API_KEY = "979f8cd5-d214-40a3-8c63-e9752332c888"
+    WORDPRESS_API_KEY = "297f1b6a4692"
+    WORDPRESS_USERNAME = "@infothmruk"
+    WORDPRESS_URL = "https://shroomni.wordpress.com"
     MARKUP_PERCENTAGE = 30
